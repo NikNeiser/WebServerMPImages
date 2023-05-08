1079e59da64a934d4160ff1a449590ba27954278
Nikolay <moshkovskiynikolay@mail.ru>
2023-05-08 17:19:53 +0300

    MainPage Front-end

---
+++ wwwroot/js/mainpage.js
@@ -0,0 +1,88 @@
+var app = (function () {
+  var config = {};
+
+  var ui = {
+    $document: $(document),
+    $cart: $("#cart"),
+    $blank: $("#blank"),
+  };
+
+  // Привязка событий
+  function _bindHandlers() {
+    ui.$document.on("click", ".cover", function () {
+      var mainCont = $(this).parents(".mainCont")[0];
+
+      $(".mainCont.opened").each(function () {
+        OpenClose(this);
+      });
+
+      OpenClose(mainCont);
+    });
+
+    ui.$document.on("click", ".close", function () {
+      var mainCont = $(this).parents(".mainCont")[0];
+      OpenClose(mainCont);
+    });
+
+    ui.$document.on("click", ".image", function () {
+      if ($(this).hasClass("selected")) {
+        var $cartItem = $("input[value='" + $(this).attr("id") + "']").closest(
+          ".cartImage"
+        );
+        $cartItem.remove();
+
+        DeselectImage($(this));
+      } else {
+        var $cartItem = ui.$blank.clone(true);
+        $cartItem.removeAttr("id");
+        $cartItem
+          .children("img")
+          .attr("src", $(this).children("img").attr("src"));
+        $cartItem.children("input").attr("value", $(this).attr("id"));
+        $cartItem.appendTo(ui.$cart);
+
+        SelectImage($(this));
+      }
+    });
+
+    ui.$document.on("click", ".deleteBtn", function () {
+      var $cartItem = $(this).closest(".cartImage");
+      $cartItem.remove();
+      var $imageBtn = $("#" + $cartItem.children("input").attr("value"));
+      DeselectImage($imageBtn);
+    });
+  }
+
+  function SelectImage($btnImage) {
+    $btnImage.closest(".mainCont").addClass("selected");
+    $btnImage.addClass("selected");
+  }
+
+  function DeselectImage($btnImage) {
+    $btnImage.removeClass("selected");
+    $imageCont = $btnImage.closest(".mainCont");
+    if ($imageCont.find(".selected").length < 1) {
+      $imageCont.removeClass("selected");
+    }
+  }
+
+  function OpenClose(mainCont) {
+    var imageCont = $(mainCont).children(".imageCont")[0];
+    $(mainCont).toggleClass("closed col col-6 col-md-2 col-sm-3 opened");
+    $(imageCont).toggleClass("row");
+  }
+
+  // Инициализация приложения
+  function init() {
+    // ...
+    _bindHandlers();
+  }
+
+  // Возвращаем наружу
+  return {
+    init: init,
+  };
+})();
+
+// Запуск приложения
+$(document).ready(app.init);
