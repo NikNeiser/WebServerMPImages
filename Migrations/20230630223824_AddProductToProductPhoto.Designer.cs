e356d71eb6d1b3c3e6525df121f4d0e587a8ecf0
Nikolay <moshkovskiynikolay@mail.ru>
2023-07-16 13:48:03 +0300

    Change ProductPhoto Table

--- Migrations/20230630223824_AddProductToProductPhoto.Designer.cs
+++ Migrations/20230630223824_AddProductToProductPhoto.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebServerMPImages.Data;
 
@@ -10,9 +11,11 @@ using WebServerMPImages.Data;
 namespace WebServerMPImages.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230630223824_AddProductToProductPhoto")]
+    partial class AddProductToProductPhoto
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
